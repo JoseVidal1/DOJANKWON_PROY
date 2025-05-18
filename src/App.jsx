f2f5426cfb9ef8ce5b441ce34a80a294d662c598
EmelDMedina <emel_d_medina@outlook.com>
2025-05-18 13:45:45 -0500

    Implementación de Login y Rutas Protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import './App.css'
-import Sidebar from './Components/sidebar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion'; // Para animaciones suaves
+import Sidebar from "./Components/sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Auth/ProtectedRoute";
 
 //import Login from './page/Login';
-import Home from './page/Home';
-import Usuarios from './page/Usuarios';
-import Reportes from './page/Reportes';
-import Productos from './page/Productos';
-import Examenes from './page/Examenes';
-import Estudiantes from './page/Estudiantes';
-
+import Estudiantes from "./page/Estudiantes";
+import Examenes from "./page/Examenes";
+import Home from "./page/Home";
+import Login from "./page/Login";
+import Pago from "./page/Pago";
+import Productos from "./page/Productos";
+import Reportes from "./page/Reportes";
+import Usuarios from "./page/Usuarios";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="flex">
-      <Sidebar />
-      <main className='flex-1 p-6 transition-all duration-300 lg:w-[77%]'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/reportes" element={<Reportes />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/examenes" element={<Examenes />} />
-          <Route path="/Estudiantes" element={<Estudiantes />} />
-        </Routes>
-      </main>
-    </div>
-  </BrowserRouter>
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-6 transition-all duration-300 lg:w-[77%]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/usuarios"
+              element={
+                <ProtectedRoute>
+                  <Usuarios />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/reportes"
+              element={
+                <ProtectedRoute>
+                  <Reportes />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/productos"
+              element={
+                <ProtectedRoute>
+                  <Productos />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/examenes"
+              element={
+                <ProtectedRoute>
+                  <Examenes />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/Estudiantes"
+              element={
+                <ProtectedRoute>
+                  <Estudiantes />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/Pago"
+              element={
+                <ProtectedRoute>
+                  <Pago />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={null} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/Components/Opcion.jsx
+++ src/Components/Opcion.jsx
@@ -1,25 +1,33 @@
-import { Sidebar } from 'lucide-react';
-import React, { useContext } from 'react';
-import { SidebarContext } from './sidebar.jsx';
-import { NavLink } from 'react-router-dom';
+import { useContext } from "react";
+import { SidebarContext } from "./sidebar.jsx";
+import { NavLink } from "react-router-dom";
 
 export default function Opcion({ icon, title, to }) {
-const  {expanded} = useContext(SidebarContext);
+  const { expanded } = useContext(SidebarContext);
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         `relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${
           isActive
-            ? 'bg-[var(--sidebar-active-bg)] text-[var(--sidebar-active-text)]'
-            : 'hover:bg-[var(--sidebar-hover-bg)] text-[var(--secundary-text-color)]'
+            ? "bg-[var(--sidebar-active-bg)] text-[var(--sidebar-active-text)]"
+            : "hover:bg-[var(--sidebar-hover-bg)] text-[var(--secundary-text-color)]"
         }`
       }
     >
-      <span className={`transition-all ${expanded ? "mr-3" : "mx-auto"}`} alt={title}>
+      <span
+        className={`transition-all ${expanded ? "mr-3" : "mx-auto"}`}
+        alt={title}
+      >
         {icon}
       </span>
-      <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0 ml-0"}`}>{title}</span>
+      <span
+        className={`overflow-hidden transition-all ${
+          expanded ? "w-52 ml-3" : "w-0 ml-0"
+        }`}
+      >
+        {title}
+      </span>
     </NavLink>
   );
 }
